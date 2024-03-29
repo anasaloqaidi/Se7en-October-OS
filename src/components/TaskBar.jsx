4bb4892a5e219f19f1c794ddfa084a40273762f6
anasaloqaidi <anasaloqaidi@gmail.com>
2024-03-29 10:49:10 +0000

    sari

--- src/components/TaskBar.jsx
+++ src/components/TaskBar.jsx
@@ -1,62 +1,57 @@
 import { Avatar, Center, Flex, Grid, Image, Modal, Overlay, Stack, Text } from '@mantine/core'
-import { useDisclosure } from '@mantine/hooks';
 import React from 'react'
+import { Animated } from 'react-animated-css';
 
-export default function TaskBar() {
-    const [opened, { open, close }] = useDisclosure(false);
+export default function TaskBar({open , opened}) {
     return (
-        <>
 
-            {opened && <Overlay onClick={close} color="#000" backgroundOpacity={0.35} blur={6}>
-                <Center h="100vh">
-                    <Grid grow w="50vw">
-                        <Grid.Col span={4}>
-                            <Stack gap={1} align="center">
-                                <Image onClick={open} src="/images/terminal.png" alt="it's me" style={{ cursor: "pointer" }} h={90}/>
-                                <Text fw={500} fz={14}>CMD</Text>
-                            </Stack>
-                        </Grid.Col>
-                        <Grid.Col span={4}>
-                            <Stack gap={1} align="center">
-                                <Image onClick={open} src="/images/terminal.png" alt="it's me" style={{ cursor: "pointer" }} h={90}/>
-                                <Text fw={500} fz={14}>CMD</Text>
-                            </Stack>
-                        </Grid.Col>
-                        <Grid.Col span={4}>
-                            <Stack gap={1} align="center">
-                                <Image onClick={open} src="/images/terminal.png" alt="it's me" style={{ cursor: "pointer" }} h={90}/>
-                                <Text fw={500} fz={14}>CMD</Text>
-                            </Stack>
-                        </Grid.Col>
-                        <Grid.Col span={4}>
-                            <Stack gap={1} align="center">
-                                <Image onClick={open} src="/images/terminal.png" alt="it's me" style={{ cursor: "pointer" }} h={90}/>
-                                <Text fw={500} fz={14}>CMD</Text>
-                            </Stack>
-                        </Grid.Col>
-                        <Grid.Col span={4}>
-                            <Stack gap={1} align="center">
-                                <Image onClick={open} src="/images/terminal.png" alt="it's me" style={{ cursor: "pointer" }} h={90}/>
-                                <Text fw={500} fz={14}>CMD</Text>
-                            </Stack>
-                        </Grid.Col>
-
-                    </Grid>
-                </Center>
-            </Overlay>}
+           
             <Flex
-                mah="5vh"
-                mih="5vh"
                 bg="rgba(0, 0, 0, .3)"
                 gap="md"
                 justify="flex-start"
                 align="center"
                 direction="row"
-                wrap="wrap"
+
             >
-                <Avatar onClick={open} src="/images/logo.webp" alt="it's me" p={3} pl={6} style={{ cursor: "pointer" }} />
+                <Animated animationIn='' animationOut='pulse' isVisible={!opened} >
+                    <Avatar
+                        onClick={open}
+                        alt="it's me" p={3} pl={6} style={{ cursor: "pointer" }}
+                        src="/images/logo.webp"
+                    />
+                </Animated>
 
 
-            </Flex></>
+                <Flex
+                    flex={1}
+                    gap="md"
+                    justify="flex-start"
+                    align="center"
+                    direction="row"
+                >
+                    <Avatar
+                        radius={0}
+                        onClick={open}
+                        alt="it's me" p={6} style={{ cursor: "pointer" }}
+                        src="/images/folder.png"
+                    />
+                </Flex>
+                <Flex
+                    gap="md"
+                    justify="flex-start"
+                    align="center"
+                    direction="row"
+                >
+                    <Avatar
+                        radius={0}
+                        onClick={open}
+                        alt="it's me" p={6} style={{ cursor: "pointer" }}
+                        src="/images/folder.png"
+                    />
+                </Flex>
+
+            </Flex>
+      
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,69 @@
-import { Image, Center, Paper } from '@mantine/core'
+import { Image, Center, Paper, Flex, Overlay, Grid, Stack, Text } from '@mantine/core'
 import React from 'react'
 import TaskBar from '../components/TaskBar'
+import { useDisclosure } from '@mantine/hooks';
 
 export default function Home() {
+    const [opened, { open, close }] = useDisclosure(false);
     return (
-        <>
-        <Center
-            h='95vh'
-            bg='#1f86a8'
+        <Flex
+            h="100vh"
+            direction="column"
         >
-            <Image
-            h={200}
-                src="/images/HomeBack.png" alt='Logo'
-            />
+            <Center
+                flex={1}
+                bg='#1f86a8'
+            >
+                <Image
+                    h={200}
+                    src="/images/HomeBack.png" alt='Logo'
+                />
 
-        </Center>
-        <TaskBar/>
-        </>
+            </Center>
+            {opened && <Overlay onClick={close} color="#000" backgroundOpacity={0.35} blur={6} >
+                <Flex
+                    h="99.94vh"
+                    direction="column"
+                >
+                    <Center flex={1}>
+                        <Grid grow w="30vw">
+                            <Grid.Col span={4}>
+                                <Stack gap={1} align="center">
+                                    <Image onClick={open} src="/images/terminal.png" alt="it's me" style={{ cursor: "pointer" }} h={90} />
+                                    <Text fw={500} fz={14}>Terminal</Text>
+                                </Stack>
+                            </Grid.Col>
+                            <Grid.Col span={4}>
+                                <Stack gap={1} align="center">
+                                    <Image onClick={open} src="/images/folder.png" alt="it's me" style={{ cursor: "pointer" }} h={90} />
+                                    <Text fw={500} fz={14}>Explorer</Text>
+                                </Stack>
+                            </Grid.Col>
+                            <Grid.Col span={4}>
+                                <Stack gap={1} align="center">
+                                    <Image onClick={open} src="/images/calendar.png" alt="it's me" style={{ cursor: "pointer" }} px={13} />
+                                    <Text fw={500} fz={14}>Calendar</Text>
+                                </Stack>
+                            </Grid.Col>
+                            <Grid.Col span={4}>
+                                <Stack gap={1} align="center">
+                                    <Image onClick={open} src="/images/terminal.png" alt="it's me" style={{ cursor: "pointer" }} h={90} />
+                                    <Text fw={500} fz={14}>CMD</Text>
+                                </Stack>
+                            </Grid.Col>
+                            <Grid.Col span={4}>
+                                <Stack gap={1} align="center">
+                                    <Image onClick={open} src="/images/terminal.png" alt="it's me" style={{ cursor: "pointer" }} h={90} />
+                                    <Text fw={500} fz={14}>CMD</Text>
+                                </Stack>
+                            </Grid.Col>
+
+                        </Grid>
+                    </Center>
+                    <TaskBar open={open} opened={opened} />
+                </Flex>
+            </Overlay>}
+            {!opened && <TaskBar open={open} opened={opened} />}
+        </Flex>
     )
 }
